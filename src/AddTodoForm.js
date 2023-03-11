eabc5d1dc828ee694c99d281cbf04708f3f3e223
Arkadiy Gornovoy <gornovoy@yahoo.com>
2023-03-11 17:04:25 -0500

    Handle "Add Todo Form" Submit

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 function AddTodoForm() {
+
+    function handleAddTodo(event) {
+        event.preventDefault();
+        const todoTitle = event.target.title.value;
+        console.log(todoTitle);
+        event.target.reset();
+    }
+
     return (
-        <form>
+        <form onSubmit={handleAddTodo}>
             <label htmlFor='todoTitle'>Title</label>
-            <input id="todoTitle"></input>
+            <input id="todoTitle" name="title"></input>
             <button>Add</button>
         </form>
     );
