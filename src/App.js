1598db72444e411a47ac84b827ff24f83db465d5
Arkadiy Gornovoy <gornovoy@yahoo.com>
2023-03-11 15:47:02 -0500

    Create "Add Todo Form" Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
 function App() {
   return (
     <div>
       <h1>Todo List</h1>
       <hr />
-      <TodoList/>
+      <AddTodoForm />
+      <TodoList />
     </div>
   );
 }
