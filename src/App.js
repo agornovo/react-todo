a4487e5c8dbb44da0a15408905cbfdbe31164aa3
Arkadiy Gornovoy <gornovoy@yahoo.com>
2023-03-11 14:08:50 -0500

    Lesson 1.1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 
+const todoList = [
+  {
+    id: 1,
+    title: "Read the book"
+  },
+  {
+    id: 2,
+    title: "Watch the video"
+  },
+  {
+    id: 3,
+    title: "Complete assignment"
+  }
+]
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <hr />
+      <ul>
+        {todoList.map(function (item) {
+          return <li key={item.id}> {item.title}</li>;
+        })}
+      </ul>
     </div>
   );
 }
